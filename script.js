74fc3df3acb15274600927baa15df152b5b07f5c
Richard <mango__82@hotmail.com>
2022-09-05 19:11:09 +1000

    add new grid function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const setGrid = document.createElement("button")
+setGrid.setAttribute("id", "set-grid")
+setGrid.innerHTML = "Set Grid Area"
+document.body.appendChild(setGrid)
+
 const container = document.createElement("div")
 container.setAttribute('id', 'grid-container')
 document.body.appendChild(container)
@@ -8,14 +13,26 @@ function appendCell(num) {
         const cell = document.createElement("div")
         const cloneCell = cell.cloneNode(true)
         cloneCell.classList.add("grid-cell")
-        container.appendChild(cloneCell) 
+        container.appendChild(cloneCell)
+        fillColor() 
         continue
     }
 }
-
-const color = document.getElementsByClassName("grid-cell")
-for (let i = 0; i < color.length; i++) {
+function fillColor() {
+    const color = document.getElementsByClassName("grid-cell")
+    for (let i = 0; i < color.length; i++) {
     color[i].addEventListener("mouseover", e => {
     color[i].classList.add("color")
 })}
-    
+}
+
+function removeAllChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+setGrid.addEventListener ("click", e => {
+    removeAllChildNodes(container)
+    appendCell(prompt ("What size grid would you like?")) 
+})
